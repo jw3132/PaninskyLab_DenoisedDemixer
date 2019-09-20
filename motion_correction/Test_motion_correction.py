721c0a4c87cf39ac150c35a7ac62490bfa3b2a39
jian wang <jian7wang@gmail.com>
2019-09-20 11:31:39 -0400

    added high pass filtering fcn

--- motion_correction/Test_motion_correction.py
+++ motion_correction/Test_motion_correction.py
@@ -1,5 +1,6 @@
 
 
+import numpy as np
 import caiman_motion_correction as mc
 
 
@@ -8,8 +9,23 @@ def test_motion_correction():
     images = mc.piecewise_rigid_registration(fnames)
     print(images.shape)
 
+def test_high_pass_filtering():
+    filepath = '/home/jian/Dropbox/LiamPaninski/locaNMF.git/locanmf/cnmfe_init_output.npz'
+    data = np.load(filepath)
+    print(data.files)
+    U = data['arr_0']
+    V = data['arr_1']
+    # A = data['arr_2']
+    # X = data['arr_3']
+    # d1, d2, T, r = data['arr_4']
+    # print(d1, d2, r, T)
+
+    Y = np.matmul(U, V)
+    Yh = mc.high_pass_filtering(Y)
+
 
 
 
 if __name__ == '__main__':
-    test_motion_correction()
+    # test_motion_correction()
+    test_high_pass_filtering()
--- motion_correction/caiman_motion_correction.py
+++ motion_correction/caiman_motion_correction.py
@@ -1,8 +1,52 @@
 
 import numpy as np
 
+import scipy
+from scipy.ndimage import gaussian_filter
+
 import caiman as cm
 from caiman.motion_correction import MotionCorrect
+
+
+def matlab_style_gauss2D(shape=(3,3),sigma=0.5):
+    """
+    2D gaussian mask - should give the same result as MATLAB's
+    fspecial('gaussian',[shape],[sigma])
+
+    :param shape: vector tuple contains 2D shape of gaussian filter
+    :param sigma: gaussian filter sigma value
+    :return: low pass gaussian filter
+    """
+    m,n = [(ss-1.)/2. for ss in shape]
+    y,x = np.ogrid[-m:m+1,-n:n+1]
+    h = np.exp( -(x*x + y*y) / (2.*sigma*sigma) )
+    h[ h < np.finfo(h.dtype).eps*h.max() ] = 0
+    sumh = h.sum()
+    if sumh != 0:
+        h /= sumh
+    return h
+
+
+def high_pass_filtering(Y, sigma=7):
+    """
+    High pass filtering
+
+    :param Y: raw image data of shape (d1, d2, T)
+    :param sigma: gaussian filter sigma value
+    :return: high pass filtered image data
+    """
+
+    gSig = sigma
+    gSize = 3 * gSig
+    psf = matlab_style_gauss2D((gSize, gSize), gSig)
+    ind_nonzero = psf >= np.amax(psf, 0)
+    psf = psf - np.mean(psf[ind_nonzero])
+    psf[psf < np.amax(psf, 0)] = 0
+    T = Y.shape[2]
+    Yh = np.zeros_like(Y)
+    for t in range(T):
+        Yh[:, :, t] = scipy.ndimage.correlate(Y[:, :, t], psf, mode='mirror')
+    return Yh
 
 
 def piecewise_rigid_registration(fnames,
@@ -13,7 +57,9 @@ def piecewise_rigid_registration(fnames,
                                  max_deviation_rigid=3,
                                  pw_rigid=False,
                                  shifts_opencv=True,
-                                 border_nan='copy'):
+                                 border_nan='copy',
+                                 highPassFilter='False',
+                                 sigma=7):
     '''Apply CaImAn piecewise rigid registration on raw imaging data
 
     :param fnames: full path filename for motion correction, possible extensions are tif, avi, npy
@@ -25,6 +71,8 @@ def piecewise_rigid_registration(fnames,
     :param pw_rigid: flag for performing rigid or piecewise rigid motion correction
     :param shifts_opencv: flag for correcting motion using bicubic interpolation (otherwise FFT interpolation is used)
     :param border_nan: replicate values along the boundary (if True, fill in with NaN)
+    :param highPassFilter: whether applying high pass filter
+    :param sigma: lowpass gaussian filter sigma value
 
     :return: motion corrected image in numpy
     '''
@@ -54,7 +102,11 @@ def piecewise_rigid_registration(fnames,
     cm.stop_server(dview=dview)  # stop the server
     m_els = cm.load(mc.fname_tot_els)
 
-    return np.array(m_els.tolist())
+    Y = np.array(m_els.tolist())
+    if highPassFilter:
+        Y = high_pass_filtering(Y.sigma)
+
+    return Y
 
 
 
