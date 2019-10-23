357fd39773c38ed577034fe2889bfb40676866b9
Ubuntu <ubuntu@ip-172-31-47-123.ec2.internal>
2019-10-23 17:25:12 +0000

    fixed run.sh, now scripts and motion correction code works

--- mc-bin/run.sh
+++ mc-bin/run.sh
@@ -24,19 +24,21 @@ mkdir -p $OUTDIR
 # Get Data & Config From Upload Bucket
 aws s3 sync s3://$1/$2/ $TMPDIR
 
-# Configure Anaconda For SSM (added by Miniconda3 4.5.12 installer)
+# >>> conda init >>>
+# !! Contents within this block are managed by 'conda init' !!
 __conda_setup="$(CONDA_REPORT_ERRORS=false '/home/ubuntu/miniconda3/bin/conda' shell.bash hook 2> /dev/null)"
 if [ $? -eq 0 ]; then
-	\eval "$__conda_setup"
+    \eval "$__conda_setup"
 else
-	if [ -f "/home/ubuntu/miniconda3/etc/profile.d/conda.sh" ]; then
-		. "/home/ubuntu/miniconda3/etc/profile.d/conda.sh"
-		CONDA_CHANGEPS1=false conda activate base
-	else
-		\export PATH="/home/ubuntu/miniconda3/bin:$PATH"
-	fi
+    if [ -f "/home/ubuntu/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/ubuntu/miniconda3/etc/profile.d/conda.sh"
+        CONDA_CHANGEPS1=false conda activate base
+    else
+        \export PATH="/home/ubuntu/miniconda3/bin:$PATH"
+    fi
 fi
 unset __conda_setup
+# <<< conda init <<<
 
 # Start Subprocess To Continuously Sync Logdir
 /home/ubuntu/bin/sync_dir.sh $LOGDIR s3://$1/$2/logs &
@@ -55,4 +57,4 @@ aws s3 sync $LOGDIR s3://$1/$2/logs/
 rm -rf $TMPDIR
 
 # Shutdown Instance
-shutdown -h now
+#shutdown -h now
